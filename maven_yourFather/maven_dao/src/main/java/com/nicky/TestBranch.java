da52c050c4f7b2f5bb6e14686544c0738eef7e39
Nickylai <497000143@qq.com>
2019-04-09 19:32:58 +0800

    添加分支mydev

---
+++ maven_yourFather/maven_dao/src/main/java/com/nicky/TestBranch.java
@@ -0,0 +1,14 @@
+package com.nicky;
+
+/**
+ * @author Nickylai
+ * @version 1.0.0
+ * @description
+ * @time 2019/4/9 19:31
+ * @see
+ */
+public class TestBranch {
+    public static void main(String[] args) {
+        System.out.println("添加mydev分支");
+    }
+}
